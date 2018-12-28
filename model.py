6e95fd5ec57b46d8cb46fdcbb5f1f7c7c53dd2bb
mo7ameds7awky <Mohamed.shawky7@fci.helwan.edu.eg>
2018-12-28 22:55:07 +0200

    Editing the model

--- model.py
+++ model.py
@@ -2,35 +2,62 @@
 """
 Created on Sat Apr 14 16:18:42 2018
 
-@author: Abdallah
+@author: Abdallah & Mohamed
 """
 
+# Imports
 import pandas as pd
 import numpy as np
+
+# Get data
 train = pd.read_csv('reg_train.csv')
+
+# Drop un-useful columns
 train =train.drop(['dteday','casual','registered'], axis=1)
 x_train = train.loc[:,'instant':'windspeed']
 y_train = train.loc[:,'cnt']
+
+# Create vector of ones
 one = np.ones([13903,1])
+
+# Convert the vector to pandas dataframe
 one = pd.DataFrame(one,dtype=int)
+
+# Concatinate the ones column to the data
 x=pd.concat([one, x_train],axis=1)
+
+# Converting data to numpy matrix for the sake of calculation on the data
 x=np.matrix(x)
 y = np.matrix(y_train)
+
+# Reshaping the data
 y = y.reshape(13903,1)
 x_t=x.reshape(14,13903)
+
+# Building the mathematical model
 xTx = np.dot(x_t,x)
 xTx_Inv = np.linalg.inv(xTx)
 xTx_Inv_xT = np.dot(xTx_Inv,x_t)
 Beta = np.dot(xTx_Inv_xT,y)
-print(Beta)
+
+# Starting the testing phase
+
+# Get test data
 test =pd.read_csv('reg_test.csv')
 inst = test.loc[:,'instant']
+
+# Drop the un-useful data
 test =test.drop(['dteday'], axis=1)
+
+# Create the ones column and concatinate it with the data
 one = np.ones([3476,1])
 one = pd.DataFrame(one,dtype=int)
 test=pd.concat([one, test],axis=1)
+
+# Converting data to numpy matrix for the sake of calculation on the data
 test=np.matrix(test)
+
+# Predict
 y_hat = np.dot(test,Beta)
 y_hat = pd.DataFrame(y_hat)
 y_hat = pd.concat([inst , y_hat] , axis = 1)
-y_hat.to_csv('reg_test_y.csv', encoding='utf-8')
